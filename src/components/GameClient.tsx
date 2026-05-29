f681195d611b1601a6bf6c83e27e2e0197381b8b
Anany Kotawala <ananykotawala@Ananys-Macbook-Pro.local>
2026-05-29 09:44:22 +0530

    cleanup

--- src/components/GameClient.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-
-import dynamic from "next/dynamic";
-
-const PhaserGame = dynamic(() => import("./PhaserGame"), { ssr: false });
-
-export default function GameClient() {
-  return <PhaserGame />;
-}
